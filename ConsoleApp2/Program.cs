951bacff4312768cbf3861d099d5eb7e7c9f9465
irfan <irfanfy@code.edu.az.com>
2022-12-31 05:17:50 +0400

    task3.3

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal n = decimal.Parse(Console.ReadLine());
+            int real = (int)n;
+            decimal fractional = n - real;
+            decimal x = (real / 1000M);
+            decimal reverse = x + fractional * 1000;
+            Console.WriteLine($"{reverse}");
+        }
+    }
+}
